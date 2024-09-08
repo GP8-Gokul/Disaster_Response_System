618ae4d50e208e77e014fa1556176196a6e0afd0
Jagannathan <jagannathan.n.1709@gmail.com>
2024-09-08 14:26:18 +0530

    Updated_select_query
    Passed it directly as JSON in key value pairs

--- Database/selectquery.py
+++ Database/selectquery.py
@@ -15,6 +15,10 @@ def get_disaster_events():
         cursor.execute("SELECT * FROM disaster_events")
         rows = cursor.fetchall()
 
+        column_names = ['event_id', 'event_name', 'event_type', 'location', 'start_date', 'end_date', 'description']
+
+        disaster_events = [dict(zip(column_names, row)) for row in rows]
+
         commit()
 
     except:
@@ -23,7 +27,7 @@ def get_disaster_events():
     finally:
         cursor.close()
 
-    return jsonify(rows)
+    return jsonify(disaster_events)
 
 def get_resources():
     cursor = get_cursor()
@@ -32,6 +36,10 @@ def get_resources():
         cursor.execute("SELECT * FROM resources")
         rows = cursor.fetchall()
 
+        column_names = ['resource_id', 'resource_name', 'resource_type', 'quantity', 'availability_status', 'event_id']
+
+        resources = [dict(zip(column_names, row)) for row in rows]
+
         commit()
 
     except:
@@ -40,7 +48,7 @@ def get_resources():
     finally:
         cursor.close()
 
-    return jsonify(rows)
+    return jsonify(resources)
 
 def get_volunteers():
     cursor = get_cursor()
@@ -49,6 +57,10 @@ def get_volunteers():
         cursor.execute("SELECT * FROM volunteers")
         rows = cursor.fetchall()
 
+        column_names = ['volunteer_id', 'volunteer_name', 'volunteer_contact_info', 'volunteer_skills', 'volunteer_availability_status', 'event_id']
+    
+        volunteers = [dict(zip(column_names, row)) for row in rows]
+
         commit()
 
     except:
@@ -57,7 +69,7 @@ def get_volunteers():
     finally:
         cursor.close()
 
-    return jsonify(rows)
+    return jsonify(volunteers)
 
 def get_aid_distribution():
     cursor = get_cursor()
@@ -66,6 +78,10 @@ def get_aid_distribution():
         cursor.execute("SELECT * FROM aid_distribution")
         rows = cursor.fetchall()
 
+        column_names = ['distribution_id','event_id', 'resource_id', 'volunteer_id', 'quantity_distributed', 'distribution_date', 'location']
+
+        aid_distribution = [dict(zip(column_names, row)) for row in rows]
+
         commit()
 
     except:
@@ -74,7 +90,7 @@ def get_aid_distribution():
     finally:
         cursor.close()
 
-    return jsonify(rows)
+    return jsonify(aid_distribution)
 
 def get_incident_reports():
     cursor = get_cursor()
@@ -83,6 +99,10 @@ def get_incident_reports():
         cursor.execute("SELECT * FROM incident_reports")
         rows = cursor.fetchall()
 
+        column_names = ['report_id', 'event_id', 'report_date', 'report_description', 'reported_by']
+
+        incident_reports = [dict(zip(column_names, row)) for row in rows]
+
         commit()
 
     except:
@@ -91,7 +111,7 @@ def get_incident_reports():
     finally:
         cursor.close()
 
-    return jsonify(rows)
+    return jsonify(incident_reports)
 
 def get_record_by_id(table, record_id):
     cursor = get_cursor()
@@ -99,6 +119,19 @@ def get_record_by_id(table, record_id):
     try:
         cursor.execute("SELECT * FROM {} WHERE id = {}".format(table, record_id))
         row = cursor.fetchone()
+
+        if(table == 'disaster_events'):
+            column_names = ['event_id', 'event_name', 'event_type', 'location', 'start_date', 'end_date', 'description']
+        elif(table == 'resources'):
+            column_names = ['resource_id', 'resource_name', 'resource_type', 'quantity', 'availability_status', 'event_id']
+        elif(table == 'volunteers'):
+            column_names = ['volunteer_id', 'volunteer_name', 'volunteer_contact_info', 'volunteer_skills', 'volunteer_availability_status', 'event_id']
+        elif(table == 'aid_distribution'):
+            column_names = ['distribution_id','event_id', 'resource_id', 'volunteer_id', 'quantity_distributed', 'distribution_date', 'location']
+        elif(table == 'incident_reports'):
+            column_names = ['report_id', 'event_id', 'report_date', 'report_description', 'reported_by']
+        
+        row = dict(zip(column_names, row))
 
         commit()
 
