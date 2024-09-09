a66b0a4615cb066645ffc594fe7e7a5d80584b08
Adith P <157714693+6e0r9e-ux@users.noreply.github.com>
2024-09-09 10:09:55 +0530

    Update insert.py

--- Database/insert.py
+++ Database/insert.py
@@ -15,31 +15,31 @@ def insert_disaster_event(event_name, event_type, location, start_date, end_date
  commit()
 
 
-def insert_resource(resource_name, resource_type, quantity, availability_status, event_id=None)
+def insert_resource(resource_name, resource_type, quantity, availability_status, event_id=None):
  cursor=get_cursor()
  cursor.execute("insert into resource values(resource_name, resource_type, quantity, availability_status, event_id=None);")
  commit()
 
 
-def insert_volunteer(name, contact_info, skills, availability_status, event_id=None)
+def insert_volunteer(name, contact_info, skills, availability_status, event_id=None):
  cursor=get_cursor()
  cursor.execute("insert into volunteer values(name, contact_info, skills, availability_status, event_id=None);")
  commit()
 
 
-def insert_aid_distribution(event_id, resource_id, volunteer_id, quantity_distributed, distribution_date, location)
+def insert_aid_distribution(event_id, resource_id, volunteer_id, quantity_distributed, distribution_date, location):
  cursor=get_cursor()
  cursor.execute("insert into aid_distribution values(event_id, resource_id, volunteer_id, quantity_distributed, distribution_date, location);")
  commit()
 
 
-def insert_user(username, password_hash, role)
+def insert_user(username, password_hash, role):
  cursor=get_cursor()
  cursor.execute("insert into user values(username, password_hash, role);")
  commit()
 
 
-def insert_incident_report(event_id, report_date, description, reported_by)
+def insert_incident_report(event_id, report_date, description, reported_by):
  cursor=get_cursor()
  cursor.execute("insert into incident_report values(event_id, report_date, description, reported_by);")
  commit()
