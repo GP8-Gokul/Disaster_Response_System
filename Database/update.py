da3c62d13cd529e3bcf76a40d2de6683a2c379e2
Gokul Krishna <167598805+Gokulrx100@users.noreply.github.com>
2024-09-09 10:53:04 +0530

    Update update.py

--- Database/update.py
+++ Database/update.py
@@ -1,3 +1,8 @@
 # update_record(table, column, value, condition_column, condition_value)
 
 from connect import get_cursor, commit;
+
+def update_record(table, column, value, condition_column, condition_value):
+    cursor = get_cursor()
+    cursor.execute(f"UPDATE {table} SET {column} = ? WHERE {condition_column} = ?", (value, condition_value))
+    commit()
