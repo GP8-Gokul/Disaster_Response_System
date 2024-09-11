226eb730895abb085277fca91db48127d9f01658
Adith P <157714693+6e0r9e-ux@users.noreply.github.com>
2024-09-11 19:27:03 +0530

    Update insert_fun_select.py

--- Backend/insert_fun_select.py
+++ Backend/insert_fun_select.py
@@ -8,27 +8,27 @@ def insert_interface(data):
 
        
         if table == 'users':
-            insert_users(data[username], data[password_hash],data[role])
+            insert_user(data['username'], data['password_hash'],data['role'])
 
 
         elif table == 'disaster_events':
-            insert_disaster_events(data[event_name], data[event_type],data[location],data[start_date],data[end_date],data[description])
+            insert_disaster_event(data['event_name'], data['event_type'],data['location'],data['start_date'],data['end_date'],data['description'])
 
 
         elif table == 'resources':
-            insert_resources(data[resource_name],data[resource_type],data[quantity],data[availability_status],data[event_id])
+            insert_resource(data['resource_name'],data['resource_type'],data['quantity'],data['availability_status'],data['event_id'])
 
 
         elif table == 'volunteers':
-             insert_volunteers(data[name],data[contact_info],data[skills],data[availability_status],data[event_id])
+             insert_volunteer(data['name'],data['contact_info'],data['skills'],data['availability_status'],data['event_id'])
 
 
         elif table == 'incident_reports':
-             insert_incident_reports(data[event_id],data[report_date],data[description],data[reported_by])
+             insert_incident_report(data['event_id'],data['report_date'],data['description'],data['reported_by'])
 
 
         elif table == 'aid_distribution':
-             insert_aid_distribution(data[event_id],data[resource_id],data[volunteer_id],data[quanity_distributed],data[distribution_date],data[location])
+             insert_aid_distribution(data['event_id'],data['resource_id'],data['volunteer_id'],data['quanity_distributed'],data['distribution_date'],data['location'])
 
 
         else:
