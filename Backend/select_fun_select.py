37645a886964015725465ebe3a790766141175e7
GP8-Gokul <gokulpjayan2004@gmail.com>
2024-09-14 21:03:25 +0530

    updated select interface logic

--- Backend/select_fun_select.py
+++ Backend/select_fun_select.py
@@ -4,7 +4,21 @@ import json
 def select_interface(data):
 
     try:
-        pass
+        if data["record_id"]:
+            return get_record_by_id(data["table"], data["record_id"])
+        if data["table"] == "disaster_events":
+            return get_disaster_events()
+        elif data["table"] == "resources":
+            return get_resources()
+        elif data["table"] == "volunteers":
+            return get_volunteers()
+        elif data["table"] == "aid_distribution":
+            return get_aid_distribution()
+        elif data["table"] == "incident_reports":
+            return get_incident_reports()
+        else:
+            return {"status": "error", "message": "Invalid table name"}
+        
 
     except Exception as e:
         return {"status": "error", "message": str(e)}
