d92d6afa2d0a3bc4ff24c7cce28a1688e4040453
Jagannathan <jagannathan.n.1709@gmail.com>
2024-09-21 11:58:00 +0530

    Changed the theme

--- Frontend/demoproject6/lib/pages/select_page.dart
+++ Frontend/demoproject6/lib/pages/select_page.dart
@@ -1,55 +1,58 @@
+import 'package:demoproject6/services/route_service/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:demoproject6/services/routes.dart';
+import 'package:demoproject6/services/image_service.dart';
 
 class SelectPage extends StatelessWidget {
   const SelectPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const Color color1 = Color.fromRGBO(3, 102, 102, 1);
-    const Color color2 = Color.fromRGBO(36, 130, 119, 1);
-    const Color color3 = Color.fromRGBO(70, 157, 137, 1);
-    const Color color4 = Color.fromRGBO(103, 185, 154, 1);
-    const Color color5 = Color.fromRGBO(136, 212, 171, 1);
+    const Color color1 = Color.fromRGBO(16, 17, 18, 1);
+    const Color color2 = Color.fromRGBO(173, 181, 189, 1);
+    const Color color3 = Color.fromRGBO(248, 249, 250, 1);
+    const Color color4 = Color.fromRGBO(52, 58, 64, 1);
+    const Color color5 = Color.fromRGBO(108, 117, 125, 1);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SELECTION'),
-        backgroundColor: color1,
-      ),
-      backgroundColor: color3,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: SingleChildScrollView(
-            // Makes the page scrollable
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                buildCard('Disaster Events', color1, () {
-                  Navigator.pushNamed(context, disaster_events);
-                }),
-                const SizedBox(height: 20),
-                buildCard('Resources', color1, () {
-                  Navigator.pushNamed(context, resources);
-                }),
-                const SizedBox(height: 20),
-                buildCard('Volunteers', color1, () {
-                  Navigator.pushNamed(context, volunteers);
-                }),
-                const SizedBox(height: 20),
-                buildCard('Aid Distribution', color1, () {
-                  Navigator.pushNamed(context, aid_distribution);
-                }),
-                const SizedBox(height: 20),
-                buildCard('Incident Reports', color1, () {
-                  Navigator.pushNamed(context, incident_reports);
-                }),
-                const SizedBox(height: 20),
-                buildCard('Record by ID', color1, () {
-                  Navigator.pushNamed(context, record_by_id);
-                }),
-              ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('SELECTION',
+              style: TextStyle(
+                color: Color.fromRGBO(248, 249, 250, 1),
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              )),
+          backgroundColor: color1,
+        ),
+        backgroundColor: color5, // White background
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildCard(disaster_event, color1, () {
+                    Navigator.pushNamed(context, disaster_events);
+                  }),
+                  const SizedBox(height: 20),
+                  buildCard(resource, color2, () {
+                    Navigator.pushNamed(context, resources);
+                  }),
+                  const SizedBox(height: 20),
+                  buildCard(volunteer, color4, () {
+                    Navigator.pushNamed(context, volunteers);
+                  }),
+                  const SizedBox(height: 20),
+                  buildCard(aid_distributions, color5, () {
+                    Navigator.pushNamed(context, aid_distributions);
+                  }),
+                  const SizedBox(height: 20),
+                  buildCard(incident_report, color1, () {
+                    Navigator.pushNamed(context, incident_reports);
+                  }),
+                ],
+              ),
             ),
           ),
         ),
@@ -57,28 +60,12 @@ class SelectPage extends StatelessWidget {
     );
   }
 
-  // Helper method to create a styled card
   Widget buildCard(String text, Color color, VoidCallback onPressed) {
     return GestureDetector(
       onTap: onPressed,
-      child: Card(
-        elevation: 8,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        color: color,
-        child: Container(
-          height: 80, // Card height
-          alignment: Alignment.center,
-          child: Text(
-            text,
-            style: const TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-        ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Image(image: AssetImage(text), fit: BoxFit.cover),
       ),
     );
   }
