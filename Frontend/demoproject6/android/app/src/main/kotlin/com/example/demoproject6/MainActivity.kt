730e4d7e161e0eaf163d1339a31fe640f46d8447
GP8-Gokul <gokulpjayan2004@gmail.com>
2024-09-22 21:32:47 +0530

    delted demoproject

--- Frontend/demoproject6/android/app/src/main/kotlin/com/example/demoproject6/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.demoproject6
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Frontend/demoproject6/lib/services/image_service.dart
+++
@@ -1,8 +0,0 @@
-// ignore: constant_identifier_names
-const disaster_event = 'assets/images/disaster_events.jpg';
-const resource = 'assets/images/resources.jpg';
-const volunteer = 'assets/images/volunteers.jpg';
-// ignore: constant_identifier_names
-const aid_distributions = 'assets/images/aid_distribution.jpg';
-// ignore: constant_identifier_names
-const incident_report = 'assets/images/incident_reports.jpg';
