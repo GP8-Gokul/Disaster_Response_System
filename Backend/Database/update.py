af33609e80ad4758cc8327b6c6b36f5f5cd4d83b
Gokul Krishna <167598805+Gokulrx100@users.noreply.github.com>
2024-09-24 20:05:54 +0530

    Update update.py

--- Backend/Database/update.py
+++ Backend/Database/update.py
@@ -12,50 +12,50 @@ def update_record(table, column, value, record_id,condition_column):
         print(f"ERROR WHILE UPDATING table : {e}")
     commit()
     
-def update_volunteer(volunteer_id, name, contact_info, skills, availability_status, event_id):
+def update_volunteer(table, volunteer_id, name, contact_info, skills, availability_status, event_id):
     cursor=get_cursor()
     try:
-        cursor.execute(f"UPDATE volunteers SET {name} = ?, {contact_info} = ?, {skills} = ?, {availability_status} = ?, {event_id} = ? WHERE {volunteer_id} = ?",(name, contact_info, skills, availability_status, event_id, volunteer_id))
+        cursor.execute(f"UPDATE {table} SET {name} = ?, {contact_info} = ?, {skills} = ?, {availability_status} = ?, {event_id} = ? WHERE {volunteer_id} = ?",(name, contact_info, skills, availability_status, event_id, volunteer_id))
         commit()
         print("UPDATED")
     except Exception as e:
         print(f"ERROR WHILE UPDATING volunteer: {e}")
 
 
-def update_disaster_event(event_id, event_name, event_type, location, start_date, end_date, description):
+def update_disaster_event(table, event_id, event_name, event_type, location, start_date, end_date, description):
     cursor = get_cursor()
     try:
-        cursor.execute(f"UPDATE disaster_events SET {event_name} = ?, {event_type} = ?, {location} = ?, {start_date} = ?, {end_date} = ?, {description} = ? WHERE {event_id} = ?",(event_name, event_type, location, start_date, end_date, description, event_id))
+        cursor.execute(f"UPDATE {table} SET {event_name} = ?, {event_type} = ?, {location} = ?, {start_date} = ?, {end_date} = ?, {description} = ? WHERE {event_id} = ?",(event_name, event_type, location, start_date, end_date, description, event_id))
         commit()
         print("UPDATED disaster_event")
     except Exception as e:
         print(f"ERROR WHILE UPDATING disaster_event: {e}")
 
 
-def update_resource(resource_id, resource_name, resource_type, quantity, availability_status, event_id):
+def update_resource(table, resource_id, resource_name, resource_type, quantity, availability_status, event_id):
     cursor = get_cursor()
     try:
-        cursor.execute(f"UPDATE resources SET {resource_name} = ?, {resource_type} = ?, {quantity} = ?, {availability_status} = ?, {event_id} = ? WHERE {resource_id} = ?",(resource_name, resource_type, quantity, availability_status, event_id, resource_id))
+        cursor.execute(f"UPDATE {table} SET {resource_name} = ?, {resource_type} = ?, {quantity} = ?, {availability_status} = ?, {event_id} = ? WHERE {resource_id} = ?",(resource_name, resource_type, quantity, availability_status, event_id, resource_id))
         commit()
         print("UPDATED resource")
     except Exception as e:
         print(f"ERROR WHILE UPDATING resource: {e}")
 
 
-def update_incident_report(report_id, event_id, report_date, description, reported_by):
+def update_incident_report(table, report_id, event_id, report_date, description, reported_by):
     cursor = get_cursor()
     try:
-        cursor.execute(f"UPDATE incident_reports SET {event_id} = ?, {report_date} = ?, {description} = ?, {reported_by} = ? WHERE {report_id} = ?",(event_id, report_date, description, reported_by, report_id))
+        cursor.execute(f"UPDATE {table} SET {event_id} = ?, {report_date} = ?, {description} = ?, {reported_by} = ? WHERE {report_id} = ?",(event_id, report_date, description, reported_by, report_id))
         commit()
         print("UPDATED incident_report")
     except Exception as e:
         print(f"ERROR WHILE UPDATING incident_report: {e}")
 
 
-def update_aid_distribution(distribution_id, event_id, resource_id, volunteer_id, quantity_distributed, distribution_date, location):
+def update_aid_distribution(table, distribution_id, event_id, resource_id, volunteer_id, quantity_distributed, distribution_date, location):
     cursor = get_cursor()
     try:
-        cursor.execute(f"UPDATE aid_distribution SET {event_id} = ?, {resource_id} = ?, {volunteer_id} = ?, {quantity_distributed} = ?, {distribution_date} = ?, {location} = ? WHERE {distribution_id} = ?",(event_id, resource_id, volunteer_id, quantity_distributed, distribution_date, location, distribution_id))
+        cursor.execute(f"UPDATE {table} SET {event_id} = ?, {resource_id} = ?, {volunteer_id} = ?, {quantity_distributed} = ?, {distribution_date} = ?, {location} = ? WHERE {distribution_id} = ?",(event_id, resource_id, volunteer_id, quantity_distributed, distribution_date, location, distribution_id))
         commit()
         print("UPDATED aid_distribution")
     except Exception as e:
