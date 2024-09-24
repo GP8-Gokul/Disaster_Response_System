aa911f20819fab9c0fef666c7c32498f2e9536fc
Gokul Krishna <167598805+Gokulrx100@users.noreply.github.com>
2024-09-24 18:56:40 +0530

    Update update_fun_select.py

--- Backend/update_fun_select.py
+++ Backend/update_fun_select.py
@@ -10,22 +10,23 @@ def update_interface(data):
         if "record_id" not in data or not data["record_id"]:
             return {"status": "error", "message": "'record_id' key is missing or empty in the request data"}, 400
 
-         
+        record_id = data["record_id"]
+
 
         if table == "disaster_events":
-            return update_disaster_event(data.get("event_name"), data.get("event_type"), data.get("location"), data.get("start_date"), data.get("end_date"), data.get("description"))
+            return update_disaster_event(record_id, data.get("event_name"), data.get("event_type"), data.get("location"), data.get("start_date"), data.get("end_date"), data.get("description"))
         
         elif table == "resources":
-            return update_resource(data.get("resource_name"), data.get("resource_type"), data.get("quantity"), data.get("availability_status"), data.get("event_id"))
+            return update_resource(record_id, data.get("resource_name"), data.get("resource_type"), data.get("quantity"), data.get("availability_status"), data.get("event_id"))
 
         elif table == "volunteers":
-            return update_volunteer(data.get("name"), data.get("contact_info"), data.get("skills"), data.get("availability_status"), data.get("event_id"))
+            return update_volunteer(record_id, data.get("name"), data.get("contact_info"), data.get("skills"), data.get("availability_status"), data.get("event_id"))
             
         elif table == "incident_reports":
-            return update_incident_report(data.get("event_id"), data.get("report_date"), data.get("description"), data.get("reported_by"))
+            return update_incident_report(record_id, data.get("event_id"), data.get("report_date"), data.get("description"), data.get("reported_by"))
             
         elif table == "aid_distribution":
-            return update_aid_distribution(data.get("event_id"),data.get("resource_id"),data.get("volunteer_id"),data.get("quantity_distributed"),data.get("distribution_date"),data.get("location"))
+            return update_aid_distribution(record_id, data.get("event_id"),data.get("resource_id"),data.get("volunteer_id"),data.get("quantity_distributed"),data.get("distribution_date"),data.get("location"))
         
         else:
             return {"status": "error", "message": "Invalid table name"}, 400
