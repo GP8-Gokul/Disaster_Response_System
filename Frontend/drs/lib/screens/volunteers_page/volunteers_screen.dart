164be9a5d5210954832b2c06b9737456239fd856
GP8-Gokul <gokulpjayan2004@gmail.com>
2024-09-24 19:35:26 +0530

    Refactor VolunteersScreen widget

--- Frontend/drs/lib/screens/volunteers_page/volunteers_screen.dart
+++ Frontend/drs/lib/screens/volunteers_page/volunteers_screen.dart
@@ -25,74 +25,10 @@ class _VolunteersScreenState extends State<VolunteersScreen> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Positioned.fill(
-          child: Image.asset(
-            'assets/images/background.jpg',
-            fit: BoxFit.cover,
-          ),
-        ),
+        buildBackgroundImage(),
         Scaffold(
-          appBar: AppBar(
-            title: const Text('Volunteers'),
-            centerTitle: true,
-            backgroundColor: Colors.transparent,
-            titleTextStyle: const TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.black
-            ),
-          ),
-          body:  FutureBuilder(
-            future: futureGetVolunteers,
-            builder: (context,snapshot) {
-              if(snapshot.connectionState == ConnectionState.waiting){
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if(snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(child: Text('No volunteers found.'));
-              } else {
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    final event = snapshot.data![index];
-                    return Container(
-                      margin: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                          title: Text("Name: ${event['volunteer_name']}"),
-                        subtitle: Text("ID: ${event['volunteer_id']}"),
-                        tileColor: Colors.white.withOpacity(0.3),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.delete, color: Colors.grey),
-                          onPressed: (){
-                          //not implemented
-                          },
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                        textColor: Colors.black,
-                        titleTextStyle: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          ),
-                        subtitleTextStyle: const TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w500,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                            side: BorderSide(color: Colors.black.withOpacity(0.0), width: 1.0),
-                            
-                          ),
-                        onTap: () {},
-                      ),
-                    );
-                  },
-                );
-              }
-            },
-          ),
+          appBar: buildAppBar(),
+          body:  buildFutureBuilder(futureGetVolunteers),
             floatingActionButton: FloatingActionButton(
             onPressed: () {
               showDialog(
@@ -176,4 +112,81 @@ class _VolunteersScreenState extends State<VolunteersScreen> {
       ],
     );
   }
+}
+
+
+Widget buildBackgroundImage() {
+  return Positioned.fill(
+    child: Image.asset(
+      'assets/images/background.jpg',
+      fit: BoxFit.cover,
+    ),
+  );
+}
+
+AppBar buildAppBar() {
+  return AppBar(
+    title: const Text('Volunteers'),
+    centerTitle: true,
+    backgroundColor: Colors.transparent,
+    titleTextStyle: const TextStyle(
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black
+    ),
+  );
+}
+
+FutureBuilder buildFutureBuilder(Future<List<Map<String, dynamic>>> futureGetVolunteers){
+  return FutureBuilder(
+    future: futureGetVolunteers,
+    builder: (context,snapshot) {
+      if(snapshot.connectionState == ConnectionState.waiting){
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      } else if(snapshot.hasError) {
+        return Center(child: Text('Error: ${snapshot.error}'));
+      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+        return const Center(child: Text('No volunteers found.'));
+      } else {
+        return ListView.builder(
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, index) {
+            final event = snapshot.data![index];
+            return Container(
+              margin: const EdgeInsets.all(8.0),
+              child: ListTile(
+                  title: Text("Name: ${event['volunteer_name']}"),
+                subtitle: Text("ID: ${event['volunteer_id']}"),
+                tileColor: Colors.white.withOpacity(0.3),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete, color: Colors.grey),
+                  onPressed: (){
+                  //not implemented
+                  },
+                ),
+                contentPadding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                textColor: Colors.black,
+                titleTextStyle: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  ),
+                subtitleTextStyle: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(color: Colors.black.withOpacity(0.0), width: 1.0),
+                    
+                  ),
+                onTap: () {},
+              ),
+            );
+          },
+        );
+      }
+    },
+  );
 }
