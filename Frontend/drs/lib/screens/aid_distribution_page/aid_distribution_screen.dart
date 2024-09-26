31f305799f71fc7b70b66d1812d62ef3a13f4014
Adith P <157714693+6e0r9e-ux@users.noreply.github.com>
2024-09-26 13:26:36 +0530

    Update aid_distribution_screen.dart

--- Frontend/drs/lib/screens/aid_distribution_page/aid_distribution_screen.dart
+++ Frontend/drs/lib/screens/aid_distribution_page/aid_distribution_screen.dart
@@ -1,4 +1,11 @@
+import 'package:drs/screens/disaster_events_page/display_disaster_events.dart';
+import 'package:drs/screens/disaster_events_page/insert_disaster_events.dart';
+import 'package:drs/screens/disaster_events_page/update_disaster_events.dart';
+import 'package:drs/services/api/disaster_event_api.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+import 'package:drs/screens/hero_dialog_route.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class AidDistributionScreen extends StatefulWidget {
   const AidDistributionScreen({super.key});
@@ -9,8 +16,234 @@ class AidDistributionScreen extends StatefulWidget {
 }
 
 class _AidDistributionScreenState extends State<AidDistributionScreen> {
+  late Future<List<Map<String, dynamic>>> futureGetAidDistribution;
+  dynamic response;
+  List<Map<String, dynamic>> allEvents = [];
+  List<Map<String, dynamic>> filteredEvents = [];
+
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    futureGetAidDistribution = fetchAidDistribution();
+    futureGetAidDistribution.then((events) {
+      setState(() {
+        allEvents = events;
+        filteredEvents = events;
+      });
+    });
+    searchController.addListener(_filterEvents);
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  void _filterEvents() {
+    final query = searchController.text.toLowerCase();
+    setState(() {
+      filteredEvents = allEvents
+          .where((event) => event['event_id'].toLowerCase().contains(query))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Aid Distribution Screen');
+    return SafeArea(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Scaffold(
+            appBar: AppBar(
+              title: const Text('Aid Distribution'),
+              centerTitle: true,
+              backgroundColor: const Color.fromARGB(100, 0, 0, 0),
+              titleTextStyle: const TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 31, 29, 29),
+              ),
+              elevation: 5,
+              shadowColor: Colors.black.withOpacity(0.3),
+            ),
+            body: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: searchController,
+                    decoration: InputDecoration(
+                      labelText: 'Search Events',
+                      hintText: 'Enter event id',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: filteredEvents.isEmpty
+                      ? const Center(
+                          child: Text(
+                            'Record not present.',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        )
+                      : ListView.builder(
+                          itemCount: filteredEvents.length,
+                          itemBuilder: (context, index) {
+                            final event = filteredEvents[index];
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8.0, horizontal: 16.0),
+                              child: Slidable(
+                                endActionPane: ActionPane(
+                                  motion: const StretchMotion(),
+                                  children: [
+                                    SlidableAction(
+                                      borderRadius: BorderRadius.circular(25),
+                                      onPressed: (context) {
+                                        devtools.log('Slide action pressed');
+                                        deleteAidDistribution(event['distribution_id']);
+                                        setState(() {
+                                          allEvents.remove(event);
+                                          filteredEvents.remove(event);
+                                        });
+                                      },
+                                      backgroundColor: const Color.fromARGB(
+                                          148, 226, 125, 125),
+                                      icon: Icons.delete,
+                                      foregroundColor:
+                                          const Color.fromARGB(255, 74, 71, 71),
+                                    ),
+                                  ],
+                                ),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    devtools.log('Event tapped');
+                                    Navigator.of(context).push(
+                                      HeroDialogRoute(
+                                        builder: (context) =>
+                                            DisplayAidDistribution(
+                                          event: event,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color:
+                                          const Color.fromARGB(255, 70, 70, 70)
+                                              .withOpacity(0.6),
+                                      borderRadius: BorderRadius.circular(25.0),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 16.0, horizontal: 24.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "Event ID: ${event['event_id']}",
+                                                style: const TextStyle(
+                                                  fontSize: 20.0,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Color.fromARGB(
+                                                      255, 41, 39, 39),
+                                                ),
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                              const SizedBox(height: 6),
+                                              Text(
+                                                "Distribution ID: ${event['distribution_id']}",
+                                                style: const TextStyle(
+                                                  fontSize: 14.0,
+                                                  color: Color.fromARGB(
+                                                      179, 47, 45, 45),
+                                                ),
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        IconButton(
+                                          icon: const Icon(
+                                            Icons.edit,
+                                            color: Colors.white,
+                                          ),
+                                          onPressed: () async {
+                                            devtools.log('Edit button pressed');
+                                            final result =
+                                                await updateAidDistributionDialog(
+                                                    context,
+                                                    fetchAidDistribution,
+                                                    event,
+                                                    response);
+                                            if (result != null) {
+                                              setState(() {
+                                                futureGetAidDistribution =
+                                                    fetchAidDistribution();
+                                                futureGetAidDistribution
+                                                    .then((events) {
+                                                  setState(() {
+                                                    allEvents = events;
+                                                    filteredEvents = events;
+                                                  });
+                                                });
+                                              });
+                                            }
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                ),
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () async {
+                devtools.log('Floating action button pressed');
+                final result = await insertAidDistributionDialog(
+                    context, fetchAidDistribution, response);
+                if (result != null) {
+                  setState(() {
+                    futureGetAidDistribution = fetchAidDistribution();
+                    futureGetAidDistribution.then((events) {
+                      setState(() {
+                        allEvents = events;
+                        filteredEvents = events;
+                      });
+                    });
+                  });
+                }
+              },
+              backgroundColor: const Color.fromARGB(255, 23, 22, 22),
+              child: const Icon(Icons.add, color: Colors.white),
+            ),
+            backgroundColor: Colors.transparent,
+          ),
+        ],
+      ),
+    );
   }
 }
