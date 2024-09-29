8ae051ad9a89375d978cd520aa2d286ef8e920d9
GP8-Gokul <gokulpjayan2004@gmail.com>
2024-09-29 21:14:43 +0530

    Refactor disaster_name_id_select function in others.py

--- Backend/Database/others.py
+++ Backend/Database/others.py
@@ -2,8 +2,7 @@ from .connect import get_cursor
 
 def disaster_name_id_select():
     cursor = get_cursor()
-    cursor.execute("SELECT event_id,event_name FROM disaster_events")
+    cursor.execute("SELECT event_id, event_name FROM disaster_events")
     rows = cursor.fetchall()
-    column_names = ['event_id', 'event_name']
-    response = [dict(zip(column_names, row)) for row in rows]
+    response = {row[0]: row[1] for row in rows}
     return response
