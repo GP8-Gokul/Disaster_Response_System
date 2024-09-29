e7b10af3284f565c7be983a510173b1f93b6c4d5
GP8-Gokul <gokulpjayan2004@gmail.com>
2024-09-29 22:59:31 +0530

    Refactor fetchEventNameId function in others_api.dart to return a Map<String, String>

--- Frontend/drs/lib/services/api/others_api.dart
+++ Frontend/drs/lib/services/api/others_api.dart
@@ -3,18 +3,15 @@ import 'dart:convert';
 import 'package:drs/services/api/root_api.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Map<String, dynamic>>> fetchEventNameId() async {
+Future<Map<String, String>> fetchEventNameId() async {
   final response = await http.get(
     Uri.parse('${url}others'),
     headers: {'Content-Type': 'application/json'},
-  
   );
+
   if (response.statusCode == 200) {
-    List<dynamic> data = jsonDecode(response.body);
-      return data.map((content) => {
-        'event_id': content['event_id'],
-        'event_name': content['event_name'],
-      }).toList();
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return Map<String, String>.from(data);
   } else {
     throw Exception('Failed to load data');
   }
