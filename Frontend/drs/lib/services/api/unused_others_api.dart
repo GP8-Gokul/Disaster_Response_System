483d2702c0885df7ea2be49de645187b855d0e60
GP8-Gokul <gokulpjayan2004@gmail.com>
2024-10-01 19:49:10 +0530

    Refactor fetchEventNameId function to return a Map<dynamic, dynamic>

--- Frontend/drs/lib/services/api/unused_others_api.dart
+++ Frontend/drs/lib/services/api/unused_others_api.dart
@@ -1,16 +1,15 @@
-
-
+import 'dart:convert';
 import 'package:drs/services/api/root_api.dart';
 import 'package:http/http.dart' as http;
 
-Future<String> fetchEventNameId() async {
+Future<Map<dynamic,dynamic>> fetchEventNameId() async {
   final response = await http.get(
     Uri.parse('${url}others'),
     headers: {'Content-Type': 'application/json'},
   );
 
   if (response.statusCode == 200) {
-    return response.body;
+    return jsonDecode(response.body);
   } else {
     throw Exception('Failed to load data');
   }
