ca7eb29337d079b68862bb6d83a732a531aab77a
GP8-Gokul <gokulpjayan2004@gmail.com>
2024-10-02 12:03:59 +0530

    Enable foreign key constraints in database connection

--- Backend/Database/connect.py
+++ Backend/Database/connect.py
@@ -11,6 +11,7 @@ db="Backend/Database/disaster.db"
 def get_cursor():
     global conn 
     conn = sqlite3.connect(db)
+    conn.execute("PRAGMA foreign_keys = ON")
     cursor=conn.cursor()
     return cursor
 
